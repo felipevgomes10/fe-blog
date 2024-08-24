6ae769b70214654aa2c102ee5ac0bd58297cfd1f
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-08-24 17:43:12 -0300

    feat: make app header sticky

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -5,14 +5,12 @@ import { useTranslations } from "next-intl";
 import LocaleSwitcher from "./locale-switcher";
 import { ThemeToggle } from "./ui/theme-toggle";
 
-type AppHeaderProps = {};
-
-export function AppHeader(props: AppHeaderProps) {
+export function AppHeader() {
   const pathname = usePathname();
   const t = useTranslations("app_header");
 
   return (
-    <nav className="flex justify-start items-center gap-4">
+    <nav className="flex justify-start items-center gap-4 sticky top-0 bg-background py-4">
       <h1 className="mr-auto scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl">
         {pathname !== "/" && <Link href="/">{t("title")}</Link>}
         {pathname === "/" && t("title")}
