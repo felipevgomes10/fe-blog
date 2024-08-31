fe937a06127cd8468228b1b9c7c857862f74256e
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-08-31 19:21:39 -0300

    refactor: organize parsePostFromApi

--- src/utils/parse-post-from-api/parse-post-from-api.ts
+++ src/utils/parse-post-from-api/parse-post-from-api.ts
@@ -8,6 +8,7 @@ export type Post = {
   slug: string;
   title: string;
   description: string;
+  content: string;
   thumbnail?: string;
 };
 
@@ -17,21 +18,34 @@ export async function parsePostFromApi({
   name,
   download_url,
 }: APIPost): Promise<Post> {
-  const slug = name.split(".").at(0) as string;
+  const slug = getPostSlug(name);
 
   const response = await fetch(download_url);
   const content = await response.text();
 
+  const { title, description, thumbnail } = getPostInformation(content);
+
+  return {
+    slug,
+    title,
+    description,
+    content,
+    thumbnail,
+  };
+}
+
+function getPostSlug(name: string): string {
+  const slug = name.split(".").at(0) as string;
+
+  return slug;
+}
+
+function getPostInformation(content: string): Omit<Post, "slug" | "content"> {
   const matchCommentRegexp = /<!--\s*([\s\S]*?)\s*-->/g;
   const matches = Array.from(content.matchAll(matchCommentRegexp));
 
   const postInformation = matches.map((match) => match.at(1)!.trim());
   const [title, description, thumbnail] = postInformation as PostInfo;
 
-  return {
-    slug,
-    title,
-    description,
-    thumbnail,
-  };
+  return { title, description, thumbnail };
 }
