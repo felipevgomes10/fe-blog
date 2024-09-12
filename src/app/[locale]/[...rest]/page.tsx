1cb1fba226d12cc523e2013570bc330a37b7cafb
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-09-12 18:43:42 -0300

    feat: change 404 page

--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -1,10 +1,24 @@
-"use client";
+import { Button } from "@/components/ui/button";
+import { Link } from "@/i18n/navigation";
+import { getTranslations } from "next-intl/server";
 
-import { useTranslations } from "next-intl";
-import Error from "next/error";
+export default async function NotFound() {
+  const t = await getTranslations("not_found");
 
-export default function NotFound() {
-  const t = useTranslations("not_found");
-
-  return <Error title={t("title")} statusCode={404} />;
+  return (
+    <section className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-accent-foreground">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+          {t("title")}
+        </h1>
+        <p className="mt-6 text-base leading-7">{t("message")}</p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Button variant="link" asChild>
+            <Link href="/">{t("back")}</Link>
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 }
