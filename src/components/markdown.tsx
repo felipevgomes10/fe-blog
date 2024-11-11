a4143e953f399c7200b160076e7950c75915838a
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-11-11 18:13:53 -0300

    feat: parse code block

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,11 +1,9 @@
 "use client";
 
+import { hljs } from "@/lib/highlight";
+import { createLine } from "@/utils/create-line/create-line";
 import type { Post } from "@/utils/parse-post-from-api/parse-post-from-api";
-import { useTranslations } from "next-intl";
-import { useEffect, useMemo } from "react";
-import { createRoot } from "react-dom/client";
 import ReactMarkdown from "react-markdown";
-import { CodeBlock } from "./code-block";
 
 import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
@@ -15,39 +13,31 @@ type MarkdownProps = {
 };
 
 export function Markdown({ content }: Readonly<MarkdownProps>) {
-  const t = useTranslations("toast");
-
-  const toastTexts = useMemo(
-    () => ({
-      success: t("success"),
-      error: t("error"),
-    }),
-    [t],
-  );
-
-  useEffect(() => {
-    const preTags = document.querySelectorAll("pre");
-
-    preTags.forEach((preTag) => {
-      const codeTag = preTag.querySelector("code");
-
-      if (!codeTag) return;
-
-      const rootElement = document.createElement("div");
-      preTag.replaceWith(rootElement);
-
-      const root = createRoot(rootElement);
-      root.render(
-        <CodeBlock
-          code={codeTag.textContent as string}
-          toastTexts={toastTexts}
-        />,
-      );
-    });
-  }, [toastTexts]);
-
   return (
-    <ReactMarkdown rehypePlugins={[rehypeRaw, rehypeSanitize]}>
+    <ReactMarkdown
+      rehypePlugins={[rehypeRaw, rehypeSanitize]}
+      components={{
+        code: (props) => {
+          const { node, children, ...rest } = props;
+
+          const { value } = hljs.highlight(children as string, {
+            language: "ts",
+          });
+
+          const codeWithLines = value
+            .split("\n")
+            .map((line, index) => createLine(line, index))
+            .join("\n");
+
+          return (
+            <code
+              {...rest}
+              dangerouslySetInnerHTML={{ __html: codeWithLines }}
+            />
+          );
+        },
+      }}
+    >
       {content}
     </ReactMarkdown>
   );
--- src/lib/highlight.ts
+++ src/lib/highlight.ts
@@ -1,18 +1,8 @@
-import { createLine } from "@/utils/create-line/create-line";
 import hljs from "highlight.js";
 import "highlight.js/styles/github-dark.min.css";
-
-hljs.addPlugin({
-  "after:highlightElement": ({ el: codeBlock, result: { value } }) => {
-    const codeWithLines = value
-      .split("\n")
-      .map((line, index) => createLine(line, index))
-      .join("\n");
-
-    codeBlock.innerHTML = codeWithLines;
-  },
-});
 
 hljs.configure({
   languages: ["typescript", "javascript", "html", "css"],
 });
+
+export { hljs };
