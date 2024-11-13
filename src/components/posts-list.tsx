18f8577fc1fcb2e37ee5a68eea9e893ac2b9f514
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-11-13 17:41:54 -0300

    feat: add search icon

--- src/components/posts-list.tsx
+++ src/components/posts-list.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { Post } from "@/utils/parse-post-from-api/parse-post-from-api";
+import { Search } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useDeferredValue, useMemo, useRef } from "react";
@@ -37,12 +38,17 @@ export function PostsList({ posts }: Readonly<PostsListProps>) {
 
   return (
     <section className="m-auto flex h-full w-full max-w-screen-xl flex-col gap-4">
-      <Input
-        className="max-w-96"
-        placeholder={t("search_placeholder")}
-        ref={inputRef}
-        onChange={handleSearch}
-      />
+      <div className="relative max-w-96">
+        <Input
+          placeholder={t("search_placeholder")}
+          ref={inputRef}
+          onChange={handleSearch}
+        />
+        <Search
+          size={15}
+          className="pointer-events-none absolute right-3 top-3 opacity-50"
+        />
+      </div>
       {filteredPosts.map(({ slug, title, description, thumbnail }) => (
         <PostCard
           key={slug}
