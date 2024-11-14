e9d5f182dea27d325c0c6e75ee0f1ef862c1a987
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-11-14 19:02:02 -0300

    feat: implement profile card

--- src/app/[locale]/about-me/page.tsx
+++ src/app/[locale]/about-me/page.tsx
@@ -1,21 +1,11 @@
 import { Markdown } from "@/components/markdown";
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
+import { ProfileCard } from "@/components/profile-card";
 import { getAboutMe } from "@/data/get-about-me";
 import {
   supportedLocales,
   type SupportedLocale,
 } from "@/i18n/supported-locales";
 import { getTranslations } from "next-intl/server";
-import Image from "next/image";
 import { redirect } from "next/navigation";
 
 type AboutMeProps = {
@@ -23,8 +13,6 @@ type AboutMeProps = {
     locale: SupportedLocale;
   };
 };
-
-const logos = ["/js.svg", "/ts.svg", "/node.svg", "/react.svg"] as const;
 
 export const revalidate = 3600; // Every hour
 
@@ -47,26 +35,7 @@ export default async function Page({
   return (
     <section className="m-auto grid max-w-screen-xl grid-cols-[max-content_1fr] gap-10">
       <aside>
-        <Card className="sticky top-[75px] max-w-72">
-          <CardHeader className="flex items-center">
-            <Avatar className="h-24 w-24">
-              <AvatarImage className="object-cover" src={aboutMe.profile} />
-            </Avatar>
-            <CardTitle>Felipe Gomes</CardTitle>
-            <CardDescription className="text-center">
-              {t("bio")}
-            </CardDescription>
-          </CardHeader>
-          <Separator className="mx-auto mb-5 w-[80%]" />
-          <CardContent>
-            <p className="leading-7 [&:not(:first-child)]:mt-6">{t("bio_2")}</p>
-          </CardContent>
-          <CardFooter className="flex items-center justify-center gap-4">
-            {logos.map((logo) => (
-              <Image key={logo} src={logo} alt={logo} width={20} height={20} />
-            ))}
-          </CardFooter>
-        </Card>
+        <ProfileCard profile={aboutMe.profile} />
       </aside>
       <div className="prose prose-slate dark:prose-invert lg:prose-xl">
         <Markdown content={aboutMe.content} />
---
+++ src/components/profile-card.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import React, { useState } from "react";
+
+type ProfileCardProps = {
+  profile: string;
+};
+
+const logos = ["/js.svg", "/ts.svg", "/node.svg", "/react.svg"] as const;
+
+export function ProfileCard({ profile }: Readonly<ProfileCardProps>) {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isHovered, setIsHovered] = useState(false);
+
+  const t = useTranslations("about_me");
+
+  function handleMouseMove(e: React.MouseEvent) {
+    const card = e.currentTarget;
+    const rect = card.getBoundingClientRect();
+
+    const x = (e.clientX - rect.left) / rect.width - 0.5;
+    const y = (e.clientY - rect.top) / rect.height - 0.5;
+
+    setPosition({ x, y });
+  }
+
+  function handleMouseEnter() {
+    setIsHovered(true);
+  }
+
+  function handleMouseLeave() {
+    setPosition({ x: 0, y: 0 });
+    setIsHovered(false);
+  }
+
+  return (
+    <Card
+      className="sticky top-[80px] z-50 max-w-72 shadow-md data-[hovered=true]:shadow-xl"
+      data-hovered={isHovered}
+      onMouseEnter={handleMouseEnter}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{
+        transform: `
+          perspective(1000px)
+          rotateY(${position.x * 15}deg)
+          rotateX(${-position.y * 15}deg)
+          scale(1.02)
+        `,
+      }}
+    >
+      <CardHeader className="flex items-center">
+        <Avatar className="h-24 w-24">
+          <AvatarImage className="object-cover" src={profile} />
+        </Avatar>
+        <CardTitle>Felipe Gomes</CardTitle>
+        <CardDescription className="text-center">{t("bio")}</CardDescription>
+      </CardHeader>
+      <Separator className="mx-auto mb-5 w-[80%]" />
+      <CardContent>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">{t("bio_2")}</p>
+      </CardContent>
+      <CardFooter className="flex items-center justify-center gap-4">
+        {logos.map((logo) => (
+          <Image key={logo} src={logo} alt={logo} width={20} height={20} />
+        ))}
+      </CardFooter>
+    </Card>
+  );
+}
