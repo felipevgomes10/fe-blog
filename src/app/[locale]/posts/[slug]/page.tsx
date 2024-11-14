4a07763ebbe39c72fb0421ba24a0a949d1ef26d7
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-11-14 12:52:48 -0300

    fix: adjust naming convention from article to post

--- src/app/[locale]/posts/[slug]/page.tsx
+++ src/app/[locale]/posts/[slug]/page.tsx
@@ -1,4 +1,5 @@
-import { Article } from "@/components/article";
+import { Markdown } from "@/components/markdown";
+import { PageNotFound } from "@/components/page-not-found";
 import { getPost } from "@/data/get-post";
 import { getPosts } from "@/data/get-posts";
 import {
@@ -7,6 +8,7 @@ import {
 } from "@/i18n/supported-locales";
 import type { Metadata } from "next";
 import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+import Image from "next/image";
 
 type Params = {
   locale: SupportedLocale;
@@ -48,8 +50,8 @@ export async function generateMetadata({
 
   if (!post) {
     return {
-      title: t("article.not_found.title"),
-      description: t("article.not_found.message"),
+      title: t("post.not_found.title"),
+      description: t("post.not_found.message"),
     };
   }
 
@@ -59,10 +61,47 @@ export async function generateMetadata({
   };
 }
 
-export default function Post({
+export default async function Post({
   params: { locale, slug },
 }: Readonly<PostProps>) {
   unstable_setRequestLocale(locale);
 
-  return <Article slug={slug} locale={locale} />;
+  const [t, post] = await Promise.all([
+    getTranslations({ locale, namespace: "post.not_found" }),
+    getPost(slug),
+  ]);
+
+  if (!post) {
+    return (
+      <PageNotFound
+        title={t("title")}
+        message={t("message")}
+        back={t("back")}
+      />
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-8">
+      {post.thumbnail && (
+        <div className="relative h-80 w-full overflow-hidden rounded-md md:h-96 lg:h-[500px]">
+          <Image
+            src={post.thumbnail}
+            alt={post.title}
+            fill
+            quality={75}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      )}
+      <div
+        className="m-auto min-h-screen max-w-max rounded-md bg-background sm:p-6 data-[translate=true]:sm:max-w-[95%] data-[translate=true]:sm:-translate-y-52 lg:data-[translate=true]:max-w-max lg:data-[translate=true]:p-12"
+        data-translate={!!post.thumbnail}
+      >
+        <article className="prose prose-slate dark:prose-invert lg:prose-xl">
+          <Markdown content={post.content} />
+        </article>
+      </div>
+    </div>
+  );
 }
