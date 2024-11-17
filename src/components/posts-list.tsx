85fabeedb3f69314e32406b5a8ede5f5a1403690
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-11-17 14:47:22 -0300

    feat: add empty message on posts list

--- src/components/posts-list.tsx
+++ src/components/posts-list.tsx
@@ -2,6 +2,7 @@
 
 import { filterPosts } from "@/utils/filter-posts/filter-posts";
 import type { Post } from "@/utils/parse-post-from-api/parse-post-from-api";
+import { useTranslations } from "next-intl";
 import { useSearchParams } from "next/navigation";
 import { useDeferredValue, useMemo } from "react";
 import { PostCard } from "./post-card";
@@ -14,10 +15,17 @@ export function PostsList({ posts }: Readonly<PostsListProps>) {
   const search = useSearchParams();
   const q = useDeferredValue(search.get("q") || "");
 
+  const t = useTranslations("app_header");
+
   const filteredPosts = useMemo(() => filterPosts({ posts, q }), [posts, q]);
 
   return (
     <section className="m-auto flex h-full min-h-[calc(100dvh_-_208px)] w-full max-w-screen-xl flex-col gap-4 pt-1">
+      {!filteredPosts.length && (
+        <p className="m-auto text-gray-500 dark:text-gray-400">
+          {t("empty")} {":("}
+        </p>
+      )}
       {filteredPosts.map(({ slug, title, description, thumbnail }) => (
         <PostCard
           key={slug}
