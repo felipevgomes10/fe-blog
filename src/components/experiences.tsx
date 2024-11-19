6829d4d9ca63ac293d397250bfcc12330cec8742
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-11-19 18:02:29 -0300

    fix: adjust experiences description styles

--- src/components/experiences.tsx
+++ src/components/experiences.tsx
@@ -32,10 +32,10 @@ export async function Experiences({ experiences }: Readonly<ExperiencesProps>) {
                 />
 
                 <div className="flex flex-col gap-2">
-                  <h4 className="text-lg leading-none text-accent-foreground">
+                  <h4 className="text-lg font-semibold leading-none text-accent-foreground">
                     {companyName}
                   </h4>
-                  <span className="text-sm leading-none text-secondary-foreground opacity-50">
+                  <span className="prose-slate text-sm leading-none opacity-50 dark:prose-invert">
                     {location}
                   </span>
                 </div>
@@ -43,21 +43,23 @@ export async function Experiences({ experiences }: Readonly<ExperiencesProps>) {
 
               <ul className="ml-8 flex list-disc flex-col gap-2">
                 {React.Children.toArray(
-                  roles.map(
-                    ({ title, startDate, endDate, description }, index) => (
-                      <li>
-                        <h5 className="text-base leading-none text-accent-foreground">
-                          {title}
-                        </h5>
-                        <span className="text-sm leading-none text-secondary-foreground opacity-50">
-                          {formatDate(startDate, locale)}
-                          {" - "}
-                          {endDate ? formatDate(endDate, locale) : t("now")}
-                        </span>
-                        <p>{description}</p>
-                      </li>
-                    ),
-                  ),
+                  roles.map(({ title, startDate, endDate, description }) => (
+                    <li className="pl-4">
+                      <h5 className="prose-slate text-base font-medium leading-none dark:prose-invert">
+                        {title}
+                      </h5>
+                      <span className="prose-slate text-sm leading-none opacity-50 dark:prose-invert">
+                        {formatDate(startDate, locale)}
+                        {" - "}
+                        {endDate ? formatDate(endDate, locale) : t("now")}
+                      </span>
+                      {description && (
+                        <p className="prose-slate mt-2 text-sm dark:prose-invert">
+                          {description}
+                        </p>
+                      )}
+                    </li>
+                  )),
                 )}
               </ul>
             </div>
