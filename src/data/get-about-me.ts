46650c5cea6a1277aab0ce3ed1c11ef7ab364dc2
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-11-19 13:15:33 -0300

    refactor: use http layer on get-about-me

--- src/data/get-about-me.ts
+++ src/data/get-about-me.ts
@@ -1,25 +1,31 @@
 "use server";
 
-import { env } from "@/env/env";
-import { api } from "@/lib/api";
-import {
-  parseAboutMeFromApi,
-  type APIAboutMe,
-} from "@/utils/parse-about-me-from-api/parse-about-me-from-api";
+import { GithubClient } from "@/http/github-client/github-client";
+import { HttpClient } from "@/http/http-client/http-client";
+import { HttpClientWithResponseAsText } from "@/http/http-client/http-client-with-response-as-text";
+import { AboutMeMapper } from "@/http/mappers/about-me-mapper/about-me-mapper";
+import { AboutMeService } from "@/http/services/about-me-service/about-me-service";
 import { getLocale } from "next-intl/server";
+
+const httpClient = HttpClient.create();
+const githubClient = GithubClient.create(httpClient);
+
+const httpClientWithResponseAsText = HttpClientWithResponseAsText.create();
+const githubClientWithResponseAsText = GithubClient.create(
+  httpClientWithResponseAsText,
+);
+
+const aboutMeService = AboutMeService.create(
+  githubClient,
+  githubClientWithResponseAsText,
+);
+const aboutMeMapper = AboutMeMapper.create();
 
 export async function getAboutMe() {
   const locale = await getLocale();
 
-  const response = await api(
-    `${env.server.GITHUB_API_URL}/felipevgomes10/contents/${locale}/about-me.md`,
-  );
+  const aboutMe = await aboutMeService.getAboutMe(locale);
+  const mappedAboutMe = await aboutMeMapper.map(aboutMe);
 
-  if (response.status === 404) return null;
-
-  const aboutMe: APIAboutMe = await response.json();
-
-  const parsedPost = parseAboutMeFromApi(aboutMe);
-
-  return parsedPost;
+  return mappedAboutMe;
 }
